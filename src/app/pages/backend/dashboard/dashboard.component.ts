f67b7b93b7d1a8f56be8af320edb1f4f4bc3e24a
LAPTOP-6A1KA1HF\Anuwat.ch <anuwatchaoyai@gmail.com>
2020-04-12 17:15:22 +0700

    first commit angular project

--- src/app/pages/backend/dashboard/dashboard.component.ts
+++ src/app/pages/backend/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../../services/product.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  dataProduct:any = []
+
+  constructor(private api: ProductService) { }
 
   ngOnInit(): void {
+    this.api.getProducts().subscribe((data:{}) => {
+      this.dataProduct = data;
+    })
   }
 
 }
--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-register',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginRegisterComponent implements OnInit {
 
-  constructor() { }
+  userData = {
+    "email": "",
+    "password":""
+  }
+  
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
 
+  submitLogin() {
+   if(this.userData.email == "admin@gmail.com" && this.userData.password == "1234"){
+      this.router.navigate(['backend']);
+   }else {
+     alert("login Fail");
+   }
+  }
 }
